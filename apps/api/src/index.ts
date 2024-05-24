f1ad849484fb50a75ef17d0a41f540ed58e4aae4
unknown <vhisoares39@gmail.com>
2024-05-24 15:54:12 -0300

    ADD: CASL Configuration/ authorization configuration

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { defineAbilityFor, projectSchema } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
+
+const project = projectSchema.parse({ id: 'project-id', ownerId: 'user2ss-id'})
+
+
+console.log(ability.can('get', 'Billing'))
+console.log(ability.can('create', 'Invite'))
+console.log(ability.can('delete', project))
